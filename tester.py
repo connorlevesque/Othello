032c1307058c70e5b11b9f8e41f5a4d6591d5066
Connor Levesque <connorlevesque5@gmail.com>
2017-12-04 12:51:31 -0500

    fix drawing problem

--- tester.py
+++ tester.py
@@ -10,6 +10,7 @@ class Tester:
         draws_wins1_wins2 = [0]*3
         for _ in range(num_games):
             game = Game(player1, player2)
+            game.play()
             windex = game.state.score()[0]
             draws_wins1_wins2[windex] += 1
         win_percent = 100 * (draws_wins1_wins2[1] / float(num_games))
